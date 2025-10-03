dd5fb3605243c13fdb1e58c9c14483b5a8e7a8b1
Samuel R <ronicksamuel@gmail.com>
2025-10-03 05:16:14 +0100

    added more tools

--- backend/src/mcp/tools/delivery_and_service/index.ts
+++ backend/src/mcp/tools/delivery_and_service/index.ts
@@ -5,7 +5,8 @@ import { AreaModel } from '../../../models/area.model';
 import { Op } from 'sequelize';
 import { BranchesModel } from '../../../models/branches.model';
 import { ZoneModel } from '../../../models/zones.model';
-
+import dayjs from 'dayjs';
+import { IArea } from '../../../types/area';
 // Get delivery options for area
 export const getDeliveryOptions = (server: McpServer) => {
   return server.registerTool(
@@ -55,6 +56,7 @@ export const getDeliveryOptions = (server: McpServer) => {
           ],
         };
       } catch (error: any) {
+        console.error(`MCP-ERROR: ${error.message}`);
         return {
           content: [
             {
@@ -83,14 +85,45 @@ export const calculateDelivery = (server: McpServer) => {
       },
     },
     async (params) => {
-      return {
-        content: [
-          {
-            type: 'text',
-            text: '',
-          },
-        ],
-      };
+      const vectorStore = new ManageVectorStore();
+      try {
+        const areas: IArea[] = (await vectorStore.searchAreas({
+          query: params.area,
+          organizationId: params.organizationId,
+          branchId: params.branchId,
+        })) as any;
+
+        if (!areas) {
+          return { content: [{ type: 'text', text: 'no area found' }] };
+        }
+        areas.forEach((d) => {
+          const diffMinutes = dayjs().diff(dayjs(d?.deliveryTime), 'minute');
+          const diffHours = dayjs().diff(dayjs(d.deliveryTime), 'hour');
+          const diffDays = dayjs().diff(dayjs(d.deliveryTime), 'day');
+          const diffMonths = dayjs().diff(dayjs(d.deliveryTime), 'month');
+
+          console.log({ diffMinutes, diffHours, diffDays, diffMonths });
+        });
+        return {
+          content: [
+            {
+              type: 'text',
+              text: JSON.stringify(areas),
+              mimeType: 'application/json',
+            },
+          ],
+        };
+      } catch (error: any) {
+        console.error(`MCP-ERROR: ${error.message}`);
+        return {
+          content: [
+            {
+              type: 'text',
+              text: 'Failed to calculate delivery time and cost for specific area and branch',
+            },
+          ],
+        };
+      }
     }
   );
 };
--- backend/src/mcp/tools/smart_recommendation/index.ts
+++ backend/src/mcp/tools/smart_recommendation/index.ts
@@ -1,5 +1,10 @@
 import { z } from 'zod';
+import { Op } from 'sequelize';
 import { McpServer } from '@modelcontextprotocol/sdk/server/mcp';
+import { ManageVectorStore } from '../../../helpers/vector-store';
+import { IArea } from '../../../types/area';
+import { BranchInventoryModel } from '../../../models/branch-inventory.model';
+import { ProductModel } from '../../../models/products.model';
 
 // Product recommendations with availability
 export const getRecommendations = (server: McpServer) => {
@@ -15,15 +20,58 @@ export const getRecommendations = (server: McpServer) => {
         maxResults: z.number().default(5),
       },
     },
-    async () => {
-      return {
-        content: [
-          {
-            type: 'text',
-            text: '',
+    async (params) => {
+      const vectorStore = new ManageVectorStore();
+      let areas: IArea[] = [];
+      try {
+        const products = (await vectorStore.searchProducts({
+          ...(params.query && { query: params.query }),
+          organizationId: params.organizationId,
+          limit: params.maxResults,
+        })) as any;
+        if (params.area) {
+          areas = (await vectorStore.searchAreas({
+            organizationId: params.organizationId,
+            query: params.area,
+            limit: params.maxResults,
+          })) as any;
+        }
+        const branchIds = areas.map((area) => area.branchId);
+        const productIds = products.map((p: any) => p?.id);
+        const availableProductsInBranch = await BranchInventoryModel.findAll({
+          where: {
+            branchId: {
+              [Op.in]: branchIds.filter(Boolean),
+            },
+            productId: {
+              [Op.in]: productIds.filter(Boolean),
+            },
+            quantityOnHand: {
+              [Op.gt]: 0,
+            },
           },
-        ],
-      };
+          limit: params.maxResults,
+        });
+        return {
+          content: [
+            {
+              type: 'text',
+              text: JSON.stringify(availableProductsInBranch),
+              mimeType: 'application/json',
+            },
+          ],
+        };
+      } catch (error: any) {
+        console.error(`MCP-ERROR: ${error.message}`);
+        return {
+          content: [
+            {
+              type: 'text',
+              text: `Failed to get products based on search, with availability status`,
+            },
+          ],
+        };
+      }
     }
   );
 };
@@ -41,15 +89,56 @@ export const suggestAlternatives = (server: McpServer) => {
         area: z.string().describe("Customer's area"),
       },
     },
-    async () => {
-      return {
-        content: [
-          {
-            type: 'text',
-            text: '',
+    async (params) => {
+      const vectorStore = new ManageVectorStore();
+      let areas: IArea[] = [];
+      try {
+        const product = await ProductModel.findByPk(params.productId);
+        if (!product) {
+          return { content: [{ type: 'text', text: 'this product does not exist' }] };
+        }
+        const text = `productName:${product.name} description: ${product.description}`;
+        const similarProducts = await vectorStore.searchProducts({
+          query: text,
+          organizationId: params.organizationId,
+        });
+        if (params.area) {
+          areas = (await vectorStore.searchAreas({ query: params.area, organizationId: params.organizationId })) as any;
+        }
+        const branchIds = areas.map((area) => area.branchId);
+        const productIds = similarProducts.map((p: any) => p?.id);
+        const availableProductsInBranch = await BranchInventoryModel.findAll({
+          where: {
+            branchId: {
+              [Op.in]: branchIds.filter(Boolean),
+            },
+            productId: {
+              [Op.in]: productIds.filter(Boolean),
+            },
+            quantityOnHand: {
+              [Op.gt]: 0,
+            },
           },
-        ],
-      };
+          limit: 10,
+        });
+        return {
+          content: [
+            {
+              type: 'text',
+              text: JSON.stringify(availableProductsInBranch),
+            },
+          ],
+        };
+      } catch (error: any) {
+        return {
+          content: [
+            {
+              type: 'text',
+              text: `Failed to get alternative products`,
+            },
+          ],
+        };
+      }
     }
   );
 };
