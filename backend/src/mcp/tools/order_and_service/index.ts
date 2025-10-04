eab16a640a583a83f34e5d5cad9306209e6db53e
Samuel R <ronicksamuel@gmail.com>
2025-10-04 08:19:18 +0100

    added more tools

--- backend/src/mcp/tools/order_and_service/index.ts
+++ backend/src/mcp/tools/order_and_service/index.ts
@@ -1,5 +1,11 @@
 import { z } from 'zod';
 import { McpServer } from '@modelcontextprotocol/sdk/server/mcp';
+import { BranchInventoryModel } from '../../../models/branch-inventory.model';
+import { Op } from 'sequelize';
+import { OrderModel } from '../../../models/order.module';
+import { IArea } from '../../../types/area';
+import { ManageVectorStore } from '../../../helpers/vector-store';
+import { BranchesModel } from '../../../models/branches.model';
 
 // Create order with inventory check
 export const createOrder = (server: McpServer) => {
@@ -10,31 +16,75 @@ export const createOrder = (server: McpServer) => {
       description: 'Create an order with inventory reservation',
       inputSchema: {
         organizationId: z.string(),
+        customerId: z.string(),
         branchId: z.string(),
-        deliveryArea: z.string().optional(),
         serviceType: z.enum(['delivery', 'takeaway']).optional(),
         items: z.array(
           z.object({
             productId: z.string(),
             quantity: z.number(),
-            options: z.array(
-              z.object({
-                items: z.string(),
-              })
-            ),
+            selectedOptions: z
+              .array(
+                z.object({
+                  optionId: z.string(),
+                  optionName: z.string(),
+                  choiceId: z.string(),
+                  choiceLabel: z.string(),
+                  priceAdjustment: z.string(),
+                })
+              )
+              .optional(),
           })
         ),
+        subtotal: z.number(),
+        deliveryCharge: z.number(),
+        totalAmount: z.number(),
+        deliveryAreaId: z.string().optional(),
+        deliveryAreaName: z.string().optional(),
       },
     },
-    async () => {
-      return {
-        content: [
-          {
-            type: 'text',
-            text: '',
+    async (params) => {
+      const products = params.items.map((item) => ({ productId: item.productId, qty: item.quantity }));
+      try {
+        const inventoryStock = await BranchInventoryModel.findAll({
+          where: {
+            productId: {
+              [Op.in]: products.map((p) => p.productId).filter(Boolean),
+            },
+            branchId: params.branchId,
           },
-        ],
-      };
+        });
+
+        if (!inventoryStock) {
+          return { content: [{ type: 'text', text: 'No inventory available for this products' }] };
+        }
+
+        for (const product of products) {
+          const inventoryItem = inventoryStock.find((inv) => inv.productId === product.productId);
+          if (!inventoryItem || inventoryItem.quantityOnHand! < product.qty) {
+            return { content: [{ type: 'text', text: `Product with ID: ${product.productId} is out of stock` }] };
+          }
+        }
+        const order = await OrderModel.create(params as any);
+        return {
+          content: [
+            {
+              type: 'text',
+              text: `Your order has being created with orderId: ${order.id}`,
+            },
+          ],
+        };
+      } catch (error: any) {
+        console.error(`MCP-ERROR:${error.message}`);
+        return {
+          content: [
+            {
+              type: 'text',
+              text: 'Failed to create order',
+            },
+          ],
+        };
+      }
     }
   );
 };
@@ -51,15 +101,45 @@ export const getBranchInfo = (server: McpServer) => {
         area: z.string().describe('Filter by area if needed').optional(),
       },
     },
-    async () => {
-      return {
-        content: [
-          {
-            type: 'text',
-            text: '',
-          },
-        ],
-      };
+    async (params) => {
+      const vectorStore = new ManageVectorStore();
+
+      try {
+        if (params.branchId) {
+          const branch = await BranchesModel.findByPk(params.branchId);
+          return { content: [{ type: 'text', text: JSON.stringify(branch) }] };
+        }
+
+        if (params.area) {
+          const areas = await vectorStore.searchAreas({
+            query: params.area,
+            organizationId: params.organizationId,
+          });
+
+          const branches = await BranchesModel.findAll({
+            where: { id: { [Op.in]: areas.map((ar: any) => ar.branchId).filter(Boolean) } },
+          });
+
+          return { content: [{ type: 'text', text: JSON.stringify(branches) }] };
+        }
+        return {
+          content: [
+            {
+              type: 'text',
+              text: 'Kindly specific the area or branch you want to search for',
+            },
+          ],
+        };
+      } catch (error: any) {
+        return {
+          content: [
+            {
+              type: 'text',
+              text: 'Failed to get branch information',
+            },
+          ],
+        };
+      }
     }
   );
 };
