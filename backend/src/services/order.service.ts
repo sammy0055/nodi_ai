c3483c36efb11b82ca712dcb0112ab72472f4087
Samuel R <ronicksamuel@gmail.com>
2026-02-19 17:55:40 +0100

    [backend]:updated dashboard

--- backend/src/services/order.service.ts
+++ backend/src/services/order.service.ts
@@ -303,7 +303,7 @@ export class OrderService {
     const stats = await OrderModel.findAll({
       attributes: ['status', [fn('COUNT', col('id')), 'count'], [fn('SUM', col('totalAmount')), 'revenue']],
       group: ['status'],
-      where: { organizationId: user.organizationId!, status: 'delivered' },
+      where: { organizationId: user.organizationId! },
     });
 
     // format result
