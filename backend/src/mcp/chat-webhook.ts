04479b4dc29d967240ab9f23bae292adc20c17c2
Samuel R <ronicksamuel@gmail.com>
2026-04-01 17:32:26 +0100

    [backend-v1]: product option fix

--- backend/src/mcp/chat-webhook.ts
+++ backend/src/mcp/chat-webhook.ts
@@ -216,7 +216,7 @@ async function handleMessages(whatsappBusinessId: string, msg: WhatsAppMessage) 
         break;
       case 'product-options-flow':
         const productOptions = response.productOptions.reduce((acc: any, item: any) => {
-          acc[item.key.replace(/\s+/g, '_')] = {
+          acc[item.key] = {
             visible: item.visible || true,
             required: item.required || false,
             label: item.label,
@@ -229,7 +229,7 @@ async function handleMessages(whatsappBusinessId: string, msg: WhatsAppMessage) 
 
         console.log('==================productOptions==================');
         console.log(productOptions);
-        console.log('=================productOptions===================');
+        console.log('====================productOptions================');
         await chat.sendWhatSappProductOptionFlowInteractiveMessage({
           recipientPhoneNumber: userPhoneNumber,
           productName: response.productName,
